a05bbe3fe871899c3ba950c88201073b5832b921
James Walker <james.chunho@gmail.com>
2022-11-07 14:40:26 +0000

    Extra print outputs in build

--- build.ps1
+++ build.ps1
@@ -1,4 +1,5 @@
-Write-Output "Building..."
+Write-Output "Building HTML..."
 pandoc --defaults config/pandoc/html.yml
+Write-Output "Building PDF..."
 pandoc --defaults config/pandoc/pdf.yml
 Write-Output "Done."
