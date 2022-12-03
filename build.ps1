6cd7c1ec7c0baa306319e1548009358fa9238b81
James Walker <james.chunho@gmail.com>
2022-12-03 22:32:53 +0000

    Generate presentation

--- build.ps1
+++ build.ps1
@@ -2,4 +2,6 @@ Write-Output "Building HTML..."
 pandoc --defaults config/pandoc/html.yml
 Write-Output "Building PDF..."
 pandoc --defaults config/pandoc/pdf.yml
+Write-Output "Building presentation..."
+pandoc --defaults config/pandoc/presentation.yml
 Write-Output "Done."
